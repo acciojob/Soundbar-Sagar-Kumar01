7e2ea06c15109df6cbfb70803c1d152ca9457f2b
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-11-23 23:45:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+let btn = document.getElementById("buttons");
+let audio = new Audio("click.mp3");
+
+btn.addEventListener("click",(e)=>{
+	if(e.target.className == "stop"){
+		
+	}else{
+		playSound();
+	}
+})
+
+function playSound() {
+    audio.currentTime = 0;
+    audio.play();
+}
+function stopSound() {
+    audio.pause();
+    audio.currentTime = 0;
+}
